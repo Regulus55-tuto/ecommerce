5d65e350890b5fe2d387cdd1cfdecc078527a65d
regulus55 <hakjoon55@gmail.com>
2024-09-23 20:47:20 +0900

    ImageRarity

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,15 +2,17 @@ import Faq from "components/Home/Faq";
 import Intro from "components/Home/Intro";
 import Main from "components/Home/Main";
 import React from "react";
+import ImageRarity from "../components/Home/ImageRarity";
 
 const Home = () => {
-  return (
-    <>
-      <Main />
-      <Intro />
-      <Faq />
-    </>
-  );
+    return (
+        <>
+            <Main/>
+            <Intro/>
+            <ImageRarity/>
+            <Faq/>
+        </>
+    );
 };
 
 export default Home;
