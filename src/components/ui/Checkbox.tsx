e0dd09f5757f927142078692a91556f67df6e848
Regulus55 <155502886+Regulus55@users.noreply.github.com>
2024-10-15 22:20:43 +0900

    signup consent

--- src/components/ui/Checkbox.tsx
+++ src/components/ui/Checkbox.tsx
@@ -1,74 +1,139 @@
+// import React from "react";
+
+// interface IProps extends React.InputHTMLAttributes<HTMLInputElement> {
+//   labelText?: string;
+//   className?: string;
+//   ariaInvalid?: boolean;
+//   type: string;
+//   error?: string;
+//   value?: string;
+//   disabled?: boolean;
+//   inputClassName?: string;
+//   htmlFor?: string;
+// }
+
+// const LABEL_CLASS = `duration absolute top-px left-5 scale-75 bg-white px-1 transition-all origin-left
+// peer-placeholder-shown:top-7 peer-placeholder-shown:left-2
+// peer-focus:top-px peer-focus:scale-75  pointer-events-none	`;
+
+// const Checkbox = React.forwardRef<HTMLInputElement, IProps>(
+//   (
+//     {
+//       labelText,
+//       className,
+//       type,
+//       ariaInvalid,
+//       error,
+//       value,
+//       disabled,
+//       inputClassName,
+//       htmlFor,
+//       ...props
+//     },
+//     ref
+//   ) => {
+//     return (
+//       <>
+//         <div
+//           className={`${error ? "animate-shake" : ""} ${
+//             className ?? ""
+//           } relative`}
+//         >
+//           <input
+//             ref={ref}
+//             {...props}
+//             className={`peer rounded-lg ${error ? "border-red-500" : ""} ${
+//               inputClassName ?? ""
+//             }`}
+//             type={type}
+//             aria-invalid={!ariaInvalid ? undefined : error ? "true" : "false"}
+//             value={value}
+//             disabled={disabled}
+//           />
+//           {/* <label htmlFor={htmlFor} className={LABEL_CLASS}>
+//             {labelText}
+//           </label> */}
+//           {error && (
+//             <small
+//               role="alert"
+//               className={`${error ? "animate-shake" : ""} text-red-500`}
+//             >
+//               {error}
+//             </small>
+//           )}
+//         </div>
+//       </>
+//     );
+//   }
+// );
+
+// Checkbox.displayName = "Checkbox";
+
+// export default Checkbox;
+
 import React from "react";
 
 interface IProps extends React.InputHTMLAttributes<HTMLInputElement> {
-    labelText?: string;
-    className?: string;
-    ariaInvalid?: boolean;
-    type: string;
-    error?: string;
-    value?: string;
-    disabled?: boolean;
-    autocomplete?: string;
-    autofocus?: boolean;
-    inputClassName?: string;
+  labelText?: string;
+  className?: string;
+  ariaInvalid?: boolean;
+  type: string;
+  error?: string;
+  value?: string;
+  disabled?: boolean;
+  inputClassName?: string;
 }
 
-const LABEL_CLASS = ` duration absolute top-px left-2 -translate-y-1/2 scale-75 bg-white px-1 transition-all origin-left	
+const LABEL_CLASS = `pl-10 duration absolute left-2 scale-75 bg-yellow-200 transition-all origin-left	
 peer-placeholder-shown:top-7 peer-placeholder-shown:left-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:text-slate-500
-peer-focus:top-px peer-focus:left-2 peer-focus:scale-75 peer-focus:text-violet-500 pointer-events-none	`;
+peer-focus:top-px  peer-focus:scale-75 peer-checked:text-violet-500 pointer-events-none	`;
 
 const Checkbox = React.forwardRef<HTMLInputElement, IProps>(
-    (
-        {
-            labelText,
-            className,
-            type,
-            ariaInvalid,
-            error,
-            value,
-            disabled,
-            autocomplete,
-            autofocus,
-            inputClassName,
-            ...props
-        },
-        ref
-    ) => {
-        return (
-            <>
-                <div
-                    className={`${error ? "animate-shake" : ""} ${
-                        className ?? ""
-                    } relative`}
-                >
-                    <label className={LABEL_CLASS}>{labelText}</label>
-                    <input
-                        ref={ref}
-                        {...props}
-                        className={` rounded-lg ${
-                            error ? "border-red-500" : ""
-                        } ${inputClassName ?? ""}`}
-                        type={type}
-                        aria-invalid={!ariaInvalid ? undefined : error ? "true" : "false"}
-                        placeholder=" "
-                        value={value}
-                        disabled={disabled}
-                        autoFocus={autofocus}
-                        autoComplete={autocomplete}
-                    />
-
-                    {error && (
-                        <small
-                            role="alert"
-                            className={`${error ? "animate-shake" : ""} text-red-500`}
-                        >
-                            {error}
-                        </small>
-                    )}
-                </div>
-            </>
-        );
-    }
+  (
+    {
+      labelText,
+      className,
+      type,
+      ariaInvalid,
+      error,
+      value,
+      disabled,
+      inputClassName,
+      ...props
+    },
+    ref
+  ) => {
+    return (
+      <>
+        <div
+          className={`${error ? "animate-shake" : ""} ${
+            className ?? ""
+          } relative`}
+        >
+          <input
+            ref={ref}
+            {...props}
+            className={`text-gray-700 peer w-full origin-left h-20 rounded-lg ${
+              error ? "border-red-500" : ""
+            } ${inputClassName ?? ""}`}
+            type={type}
+            aria-invalid={!ariaInvalid ? undefined : error ? "true" : "false"}
+            value={value}
+            disabled={disabled}
+          />
+          <label className={LABEL_CLASS}>{labelText}</label>
+          {error && (
+            <small
+              role="alert"
+              className={`${error ? "animate-shake" : ""} text-red-500`}
+            >
+              {error}
+            </small>
+          )}
+        </div>
+      </>
+    );
+  }
 );
 
 Checkbox.displayName = "Checkbox";
--- src/pages/Auth/Signup.tsx
+++ src/pages/Auth/Signup.tsx
@@ -1,221 +1,210 @@
-import {AuthImageContainer, Button, Input} from "components/ui";
-import {EMAIL_REGEX} from "data/Auth/authData";
-import React from "react";
-import {useForm} from "react-hook-form";
-import {Link, useNavigate} from "react-router-dom";
+import { AuthImageContainer, Button, Input } from "components/ui";
+import { EMAIL_REGEX } from "data/Auth/authData";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
 import Checkbox from "../../components/ui/Checkbox";
 import axios from "axios";
 
 interface IProps {
-    username: string;
-    email: string;
-    password: string;
-    confirmPassword: string;
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
 
-    overTwenty: boolean;
-    agreeOfTerm: boolean;
-    agreeOfPersonalInfo: boolean;
-    agreeOfMarketing: boolean;
-    etc: boolean;
-
+  overTwenty: boolean;
+  agreeOfTerm: boolean;
+  agreeOfPersonalInfo: boolean;
+  agreeOfMarketing: boolean;
+  etc: boolean;
 }
 
 const Signup = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const {
-        register,
-        handleSubmit,
-        formState: {isSubmitting, isDirty, errors},
-        setError,
-    } = useForm<IProps>({
-        mode: "onSubmit",
-        defaultValues: {
-            username: "",
-            email: "",
-            password: "",
-            confirmPassword: "",
-            overTwenty: false,
-            agreeOfTerm: false,
-            agreeOfPersonalInfo: false,
-            agreeOfMarketing: false,
-            etc: false,
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, isDirty, errors },
+    setError,
+  } = useForm<IProps>({
+    mode: "onSubmit",
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      overTwenty: false,
+      agreeOfTerm: false,
+      agreeOfPersonalInfo: false,
+      agreeOfMarketing: false,
+      etc: false,
+    },
+  });
 
-        },
-    });
+  const submit = async (data: IProps) => {
+    console.log(data);
 
-    const submit = async (data: IProps) => {
-        console.log(data);
+    try {
+      // const userInput = {
+      //     username: data.username,
+      //     email: data.email,
+      //     password: data.password,
+      // }
+      const url = "http://localhost:8000/api/auth/signup";
+      const { status } = await axios.post(url, data);
+      if (status === 201) {
+        alert("signup ss");
+        navigate("/login");
+      }
+    } catch (e) {
+      console.log("errorrrr", e);
+    }
+  };
 
-        try{
-            // const userInput = {
-            //     username: data.username,
-            //     email: data.email,
-            //     password: data.password,
-            // }
-            const url = 'http://localhost:8000/api/auth/signup'
-            const { status} = await axios.post(url, data)
-            if(status === 201){
-                alert('signup ss')
-                navigate('/login')
-            }
-        }catch(e){
-            console.log('errorrrr',e)
-        }
-    };
+  const agreements = [
+    { id: 1, label: "14세 이상입니다 (필수)", key: "overTwenty" },
+    { id: 2, label: "이용약관(필수)", key: "agreeOfTerm" },
+    {
+      id: 3,
+      label: "개인정보수집 및 이용동의 (필수)",
+      key: "agreeOfPersonalInfo",
+    },
+    {
+      id: 4,
+      label: "개인정보 마케팅 활용 동의 (선택)",
+      key: "agreeOfMarketing",
+    },
+    { id: 5, label: "이벤트, 특가 알림 및 SMS 등 수신 (선택)", key: "etc" },
+  ];
 
-    const agreements = [
-        {id: 1, label: "14세 이상입니다(필수)", key: "overTwenty"},
-        {id: 2, label: "이용약관(필수)", key: "agreeOfTerm"},
-        {id: 3, label: "개인정보수집 및 이용동의(필수)", key: "agreeOfPersonalInfo"},
-        {id: 4, label: "개인정보 마케팅 활용 동의(선택)", key: "agreeOfMarketing"},
-        {id: 5, label: "이벤트, 특가 알림 및 SMS 등 수신(선택)", key: "etc"}
-    ];
-
-    return (
-        <section className="m-auto grid min-h-[calc(100vh-65px)] w-full grid-cols-10">
-            <div
-                className="col-span-10 flex h-full w-full grow flex-col items-center justify-center bg-white shadow-slate-50 drop-shadow-md lg:col-span-4">
-                <div className="mb-14 flex-col items-center text-center">
-                    <h2 className="mb-2 text-3xl font-bold">Welcome!</h2>
-                    <p className="text-slate-500">for your first visit! </p>
-                </div>
-                <div className="flex w-full flex-col items-center">
-                    <form
-                        className="flex w-full max-w-sm flex-col"
-                        onSubmit={handleSubmit((data) => submit(data))}
-                    >
-                        <Input
-                            {...register("username", {
-                                required: "Please provide an Username",
-                            })}
-                            error={errors.username?.message}
-                            ariaInvalid={isDirty}
-                            labelText="Username"
-                            type="text"
-                            className={"mb-3"}
-                            autocomplete="on"
-                            autofocus
-                        />
-
-                        <Input
-                            {...register("email", {
-                                required: "Please provide an email",
-                                pattern: {
-                                    value: EMAIL_REGEX,
-                                    message: "Please provide a properly formatted email address",
-                                },
-                            })}
-                            error={errors.email?.message}
-                            ariaInvalid={isDirty}
-                            labelText="Email"
-                            type="email"
-                            className="mb-3"
-                            autocomplete="on"
-                        />
-
-                        <Input
-                            {...register("password", {
-                                required: "Please provide a password",
-                                minLength: {
-                                    value: 6,
-                                    message: "Password needs to be between 6 to 20 characters",
-                                },
-                                maxLength: {
-                                    value: 20,
-                                    message: "Password needs to be between 6 to 20 characters",
-                                },
-                            })}
-                            error={errors.password?.message}
-                            ariaInvalid={isDirty}
-                            labelText="Password"
-                            type="password"
-                            className="mb-3"
-                            autocomplete="off"
-                        />
-
-                        <Input
-                            {...register("confirmPassword", {
-                                required: "Please provide a confirm password",
-                            })}
-                            error={errors.confirmPassword?.message}
-                            ariaInvalid={isDirty}
-                            labelText={"Confirm password"}
-                            type={"password"}
-                            className={"mb-10"}
-                            autocomplete="off"
-                        />
-
-                        <div className={'border border-gray-500 mb-4'}>
-
-
-                            <Checkbox type={'checkbox'} {...register('overTwenty')} labelText={"14세 이상입니다(필수)"}/>
-                            <Checkbox type={'checkbox'} {...register('agreeOfTerm')} labelText={"이용약관(필수)"}/>
-                            <Checkbox type={'checkbox'} {...register('agreeOfPersonalInfo')}
-                                      labelText={"개인정보수집동의(필수)"}/>
-                            <Checkbox type={'checkbox'} {...register('agreeOfMarketing')}
-                                      labelText={"개인정보마케팅 활용동의(선택)"}/>
-                            <Checkbox type={'checkbox'} {...register('etc')} labelText={"이벤트 및 SMS 등 수신(선택)"}/>
-
-
-                            {/*<Input type={'checkbox'} {...register('.agreeOfTerm')} labelText="Agree of Term" />*/}
-
-                            {/*  {...register("consent.agreeOfTerm", {*/}
-                            {/*    required: "Please check Consent Terms",*/}
-                            {/*  })}*/}
-                            {/*  error={errors.consent?.message}*/}
-                            {/*  ariaInvalid={isDirty}*/}
-                            {/*  labelText={"Agree Of Term"}*/}
-                            {/*  type={"checkbox"}*/}
-                            {/*  className={"mb-10"}*/}
-                            {/*  autocomplete="off"*/}
-                            {/*/>*/}
-                            {/*<Input*/}
-                            {/*    {...register("consent.agreeOfPersonalInfo", {*/}
-                            {/*      required: "Please check Consent Terms",*/}
-                            {/*    })}*/}
-                            {/*    error={errors.consent?.message}*/}
-                            {/*    ariaInvalid={isDirty}*/}
-                            {/*    labelText={"Agree Of Personal Info"}*/}
-                            {/*    type={"checkbox"}*/}
-
-                            {/*    autocomplete="off"*/}
-                            {/*/>*/}
-                            {/*  <Input*/}
-                            {/*      {...register("consent.agreeOfPersonalInfo", {*/}
-                            {/*        required: "Please check Consent Terms",*/}
-                            {/*      })}*/}
-                            {/*      error={errors.consent?.message}*/}
-                            {/*      ariaInvalid={isDirty}*/}
-                            {/*      labelText={"Agree Of Personal Info"}*/}
-                            {/*      type={"checkbox"}*/}
-                            {/*      className={"mb-10"}*/}
-                            {/*      autocomplete="off"*/}
-                            {/*  />*/}
-                        </div>
-
-                        <Button
-                            text={"Signup"}
-                            disabled={isSubmitting}
-                            className="rounded-lg bg-violet-500 py-4 font-semibold text-white hover:bg-violet-600"
-                        />
-                    </form>
-
-                    <div className={"mt-10 text-slate-500"}>
-                        Already have an account?
-                        <Link to={"/login"} className={"p-2 font-semibold text-violet-500"}>
-                            Log in
-                        </Link>
-                    </div>
-                </div>
-            </div>
-            <AuthImageContainer
-                image={"/images/register.webp"}
-                firstText="shop smarter"
-                secondText="Signup here"
+  return (
+    <section className="m-auto grid min-h-[calc(100vh-65px)] w-full grid-cols-10">
+      <div className="col-span-10 flex h-full w-full grow flex-col items-center justify-center bg-white shadow-slate-50 drop-shadow-md lg:col-span-4">
+        <div className="mb-14 flex-col items-center text-center">
+          <h2 className="mb-2 text-3xl font-bold">Welcome!</h2>
+          <p className="text-slate-500">for your first visit! </p>
+        </div>
+        <div className="flex w-full flex-col items-center">
+          <form
+            className="flex w-full max-w-sm flex-col"
+            onSubmit={handleSubmit((data) => submit(data))}
+          >
+            <Input
+              {...register("username", {
+                required: "Please provide an Username",
+              })}
+              error={errors.username?.message}
+              ariaInvalid={isDirty}
+              labelText="Username"
+              type="text"
+              className={"mb-3"}
+              autocomplete="on"
+              autofocus
             />
-        </section>
-    );
+
+            <Input
+              {...register("email", {
+                required: "Please provide an email",
+                pattern: {
+                  value: EMAIL_REGEX,
+                  message: "Please provide a properly formatted email address",
+                },
+              })}
+              error={errors.email?.message}
+              ariaInvalid={isDirty}
+              labelText="Email"
+              type="email"
+              className="mb-3"
+              autocomplete="on"
+            />
+
+            <Input
+              {...register("password", {
+                required: "Please provide a password",
+                minLength: {
+                  value: 6,
+                  message: "Password needs to be between 6 to 20 characters",
+                },
+                maxLength: {
+                  value: 20,
+                  message: "Password needs to be between 6 to 20 characters",
+                },
+              })}
+              error={errors.password?.message}
+              ariaInvalid={isDirty}
+              labelText="Password"
+              type="password"
+              className="mb-3"
+              autocomplete="off"
+            />
+
+            <Input
+              {...register("confirmPassword", {
+                required: "Please provide a confirm password",
+              })}
+              error={errors.confirmPassword?.message}
+              ariaInvalid={isDirty}
+              labelText={"Confirm password"}
+              type={"password"}
+              className={"mb-10"}
+              autocomplete="off"
+            />
+
+            <div
+              className={
+                "border peer rounded-lg py-2 px-4 border-gray-300 mb-4"
+              }
+            >
+              {/* {agreements.map((item) => (
+                <div
+                  className="flex items-center space-x-2 text-gray-500"
+                  key={item.id}
+                >
+                  <input
+                    type="checkbox"
+                    id={item.key}
+                    {...register(item.key as keyof IProps)}
+                    className="hidden peer"
+                  />
+                  <div className="flex items-center justify-center w-5 h-5 border-2 border-gray-400 rounded-lg peer-checked:bg-violet-500 peer-checked:border-violet-300" />
+                  <label htmlFor={item.key}>{item.label}</label>
+                </div>
+              ))} */}
+
+              {agreements.map((item) => (
+                <Checkbox
+                  type="checkbox"
+                  {...register(item.key as keyof IProps, {
+                    required: "Please provide a password",
+                  })}
+                  labelText={item.label}
+                  id={item.key}
+                />
+              ))}
+            </div>
+
+            <Button
+              text={"Signup"}
+              disabled={isSubmitting}
+              className="rounded-lg bg-violet-500 py-4 font-semibold text-white hover:bg-violet-600"
+            />
+          </form>
+
+          <div className={"mt-10 text-slate-500"}>
+            Already have an account?
+            <Link to={"/login"} className={"p-2 font-semibold text-violet-500"}>
+              Log in
+            </Link>
+          </div>
+        </div>
+      </div>
+      <AuthImageContainer
+        image={"/images/register.webp"}
+        firstText="shop smarter"
+        secondText="Signup here"
+      />
+    </section>
+  );
 };
 
 export default Signup;
