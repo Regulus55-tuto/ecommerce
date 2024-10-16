ee6dbdfe8b321db9ac3fc46d74c420d393dc5281
regulus55 <hakjoon55@gmail.com>
2024-10-16 11:48:12 +0900

    consent

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,33 +1,35 @@
 import React from "react";
 
 interface ButtonPros {
-  text: string;
-  onClick?: (e?: any) => void;
-  disabled?: boolean;
-  className?: string;
-  icon?: IconType;
+    text: string;
+    onClick?: (e?: any) => void;
+    disabled?: boolean;
+    className?: string;
+    icon?: IconType;
+    type?: string;
 }
 
 type IconType = () => JSX.Element;
 
 export default function Button({
-  text,
-  onClick,
-  disabled,
-  className,
-  icon,
-}: ButtonPros) {
-  return (
-    <button
-      onClick={onClick}
-      className={`flex max-h-[44px] items-center justify-center rounded-md transition-all ${
-        className ?? ""
-      }`}
-      disabled={disabled}
-      aria-label={text}
-    >
-      {icon && React.createElement(icon)}
-      {text}
-    </button>
-  );
+                                   text,
+                                   onClick,
+                                   disabled,
+                                   className,
+                                   icon,
+                                   type,
+                               }: ButtonPros) {
+    return (
+        <button
+            onClick={onClick}
+            className={`flex max-h-[44px] items-center justify-center rounded-md transition-all ${
+                className ?? ""
+            }`}
+            disabled={disabled}
+            aria-label={text}
+        >
+            {icon && React.createElement(icon)}
+            {text}
+        </button>
+    );
 }
