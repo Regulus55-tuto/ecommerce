9fbd083d03c791a07b9b25c6e27017de09c14f41
Regulus55 <155502886+Regulus55@users.noreply.github.com>
2024-10-21 23:55:54 +0900

    collectionsData

--- src/pages/Collections.tsx
+++ src/pages/Collections.tsx
@@ -1,182 +1,126 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import ProductTitle from "../components/ui/ProductTitle";
-import {Menu} from "@headlessui/react";
-import {ChevronDownIcon, FunnelIcon} from "@heroicons/react/16/solid";
+import { Menu } from "@headlessui/react";
+import { ChevronDownIcon, FunnelIcon } from "@heroicons/react/16/solid";
 import CategorySort from "../components/Product/CategorySort";
 import useSortParams from "../hooks/useSortParams";
 import FilterDesktop from "../components/Product/FilterDesktop";
-import {COLLECTION, FILTERS} from "../data/Products";
-import {ProductType} from "../utiles/interfaces";
+import { COLLECTION, FILTERS } from "../data/Products";
+import { ProductType } from "../utiles/interfaces";
 import ProductCard3 from "../components/ui/ProductCard3";
-
+import { accessory, computer, smartphone } from "data/Products/collectionsData";
 
 const Collections = () => {
-    const {query, searchParams, setSortParams, deleteSortParams} = useSortParams()
+  const { query, searchParams, setSortParams, deleteSortParams } =
+    useSortParams();
 
-    // Get Filter Options
-    const [colors, setColors] = useState<string[]>([]);
-    const [sizes, setSizes] = useState<string[]>([])
-    const [products, setProducts] = useState<ProductType[]>([
-        {
-            title: 'Galaxy Flip6',
-            price: 1300000,
-            category: 'smartphone',
-            tags: 'flip-series',
-            colors: ["black","white","violet"],
-            description: 'The latest flip-serires',
-            options: 'flip',
-            id: 1,
-            image: ['https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQTzPHbe9xsqJcVsojt5VKj2PlTtl5ju3AjpnClrS-eIqnbogKD6rlp7MfM4Pgb8RYdrVR1Pf8MTnluTIpjZmMuC1fX6fV2rSWIUIQJ4YCfpJV_3cd50T6i8z6Mf3K2V6p8v1tGKw&usqp=CAc'],
-            details: 'New Flip Smartphone',
-            highlights: [
-                "Galaxy Ai","5천만 화소","콤팩트한 사이즈","나만의 유니크 스타일"
-            ]
-        },
-        {
-            title: 'Galaxy Fold6',
-            price: 2400000,
-            category: 'smartphone',
-            tags: 'fold-series',
-            colors: ["black","white","violet"],
-            description: 'The latest fold-serires',
-            options: 'fold',
-            id: 1,
-            image: ['https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQTzPHbe9xsqJcVsojt5VKj2PlTtl5ju3AjpnClrS-eIqnbogKD6rlp7MfM4Pgb8RYdrVR1Pf8MTnluTIpjZmMuC1fX6fV2rSWIUIQJ4YCfpJV_3cd50T6i8z6Mf3K2V6p8v1tGKw&usqp=CAc'],
-            details: 'New Flip Smartphone',
-            highlights: [
-                "Galaxy Ai","5천만 화소","콤팩트한 사이즈","나만의 유니크 스타일"
-            ]
-        },
-        {
-            title: 'Galaxy Flip6',
-            price: 1300000,
-            category: 'smartphone',
-            tags: 'flip-series',
-            colors: ["black","white","violet"],
-            description: 'The latest flip-serires',
-            options: 'flip',
-            id: 1,
-            image: ['https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQTzPHbe9xsqJcVsojt5VKj2PlTtl5ju3AjpnClrS-eIqnbogKD6rlp7MfM4Pgb8RYdrVR1Pf8MTnluTIpjZmMuC1fX6fV2rSWIUIQJ4YCfpJV_3cd50T6i8z6Mf3K2V6p8v1tGKw&usqp=CAc'],
-            details: 'New Flip Smartphone',
-            highlights: [
-                "Galaxy Ai","5천만 화소","콤팩트한 사이즈","나만의 유니크 스타일"
-            ]
-        },
-        {
-            title: 'Galaxy Fold6',
-            price: 2400000,
-            category: 'smartphone',
-            tags: 'fold-series',
-            colors: ["black","white","violet"],
-            description: 'The latest fold-serires',
-            options: 'fold',
-            id: 1,
-            image: ['https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQTzPHbe9xsqJcVsojt5VKj2PlTtl5ju3AjpnClrS-eIqnbogKD6rlp7MfM4Pgb8RYdrVR1Pf8MTnluTIpjZmMuC1fX6fV2rSWIUIQJ4YCfpJV_3cd50T6i8z6Mf3K2V6p8v1tGKw&usqp=CAc'],
-            details: 'New Flip Smartphone',
-            highlights: [
-                "Galaxy Ai","5천만 화소","콤팩트한 사이즈","나만의 유니크 스타일"
-            ]
-        }
-    ])
+  // Get Filter Options
+  const [colors, setColors] = useState<string[]>([]);
+  const [sizes, setSizes] = useState<string[]>([]);
+  const [products, setProducts] = useState<ProductType[]>([
+    ...smartphone.aSeries,
+    ...smartphone.sSeries,
+  ]);
 
-    return (
-        <div className={'bg-white'}>
+  return (
+    <div className={"bg-white"}>
+      <main className={"mx-auto mb-32 max-w-7xl px-4 sm:px-6 lg:px-8"}>
+        {/* Title, Breadcrumbs, Sort */}
+        <div className="flex items-end justify-between border-b border-gray-200 pt-24 pb-6">
+          <div className="flex flex-col">
+            {/* page title */}
+            <ProductTitle title={"Shop All"} />
+          </div>
+          <div className={"flex items-center"}>
+            <Menu as={"div"} className={"relative inline-block text-left"}>
+              <div>
+                <Menu.Button className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
+                  Sort
+                  <ChevronDownIcon
+                    className="-mr-1 ml-1 h-5 w-5 shrink-0 text-gray-400 group-hover:text-gray-500"
+                    aria-hidden="true"
+                  />
+                </Menu.Button>
+              </div>
 
+              <CategorySort
+                setSortParams={setSortParams}
+                deleteSortParams={deleteSortParams}
+                sortParams={searchParams.get("sort")}
+                ascParams={searchParams.get("asc")}
+              />
+            </Menu>
 
-            <main className={'mx-auto mb-32 max-w-7xl px-4 sm:px-6 lg:px-8'}>
-                {/* Title, Breadcrumbs, Sort */}
-                <div className="flex items-end justify-between border-b border-gray-200 pt-24 pb-6">
-                    <div className="flex flex-col">
-                        {/* page title */}
-                        <ProductTitle title={"Shop All"}/>
-                    </div>
-                    <div className={'flex items-center'}>
-                        <Menu as={"div"} className={'relative inline-block text-left'}>
-                            <div>
-                                <Menu.Button
-                                    className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
-                                    Sort
-                                    <ChevronDownIcon
-                                        className="-mr-1 ml-1 h-5 w-5 shrink-0 text-gray-400 group-hover:text-gray-500"
-                                        aria-hidden="true"
-                                    />
-                                </Menu.Button>
-                            </div>
-
-                            <CategorySort
-                                setSortParams={setSortParams}
-                                deleteSortParams={deleteSortParams}
-                                sortParams={searchParams.get('sort')}
-                                ascParams={searchParams.get('asc')}
-                            />
-                        </Menu>
-
-                        <button
-                            className={'-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden'}
-                        >
-                            <span className={'sr-only'}>Filters</span>
-                            <FunnelIcon className={"h-5 w-5"} aria-hidden="true"/>
-                        </button>
-                    </div>
-                </div>
-
-                {/*{filter}*/}
-                <section aria-labelledby={'products-heading'} className={'pt-6 pb-24'}>
-                    <h2 id={'products-heading'} className={'sr-only'}>
-                        Products
-                    </h2>
-                    <div className={'grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4'}>
-
-                        {/*filterBox*/}
-                        <FilterDesktop
-                            allCategories={COLLECTION}
-                            filters={FILTERS}
-                            colors={colors}
-                            sizes={sizes}
-                            setSortParams={setSortParams}
-                            deleteSortParams={deleteSortParams}
-                            searchParams={searchParams}
-                        />
-
-                        <div className={'lg:col-span-3'}>
-                            <ul className={'grid grid-cols-1 gap-6 md:grid-cols-2'}>
-                                <>
-                                    {}
-                                </>
-                            </ul>
-
-                            <ul className={'grid grid-cols-1 gap-6 md:grid-cols-2'}>
-                                <>
-                                    {products?.map((product)=>(
-                                        <li
-                                            key={product.id}
-                                            className={'group cursor-pointer rounded-2xl transition-shadow duration-300 ease-in-out hover:shadow-md'}
-                                        >
-                                            <ProductCard3
-                                                image={product.image}
-                                                title={product.title}
-                                                id={product.id}
-                                                price={product.price}
-                                                category={product.category}
-                                                tags={product.tags}
-                                                colors={product.colors}
-                                                description={product.description}
-                                                options={product.options}
-                                                details={product.details}
-
-                                            />
-                                        </li>
-                                    ))}
-                                </>
-                            </ul>
-                        </div>
-
-
-                    </div>
-                </section>
-            </main>
+            <button
+              className={
+                "-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden"
+              }
+            >
+              <span className={"sr-only"}>Filters</span>
+              <FunnelIcon className={"h-5 w-5"} aria-hidden="true" />
+            </button>
+          </div>
         </div>
-    );
+
+        {/*{filter}*/}
+        <section aria-labelledby={"products-heading"} className={"pt-6 pb-24"}>
+          <h2 id={"products-heading"} className={"sr-only"}>
+            Products
+          </h2>
+          <div className={"grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4"}>
+            {/*filterBox*/}
+            <FilterDesktop
+              allCategories={COLLECTION}
+              filters={FILTERS}
+              colors={colors}
+              sizes={sizes}
+              setSortParams={setSortParams}
+              deleteSortParams={deleteSortParams}
+              searchParams={searchParams}
+            />
+
+            <div className={"lg:col-span-3"}>
+              <ul className={"grid grid-cols-1 gap-6 md:grid-cols-2"}>
+                <>{}</>
+              </ul>
+
+              <ul
+                className={
+                  "grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3"
+                }
+              >
+                <>
+                  {products?.map((product) => (
+                    <li
+                      key={product.id}
+                      className={
+                        "group cursor-pointer rounded-2xl transition-shadow duration-300 ease-in-out hover:shadow-md"
+                      }
+                    >
+                      <ProductCard3
+                        image={product.image}
+                        storage={product.storage}
+                        model={product.model}
+                        title={product.title}
+                        id={product.id}
+                        referencePrice={product.referencePrice}
+                        promotionalPrice={product.promotionalPrice}
+                        category={product.category}
+                        tags={product.tags}
+                        colors={product.colors}
+                        description={product.description}
+                        details={product.details}
+                      />
+                    </li>
+                  ))}
+                </>
+              </ul>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 };
 
 export default Collections;
--- src/utiles/interfaces.ts
+++ src/utiles/interfaces.ts
@@ -1,40 +1,43 @@
 //Filters
 export interface FilterPropsType {
-    isLoading?: boolean;
-    allCategories: string[];
-    filters: filterType[];
-    colors: string[];
-    sizes: string[];
-    setSortParams: (key: string, value: string) => void;
-    deleteSortParams: (key: string) => void;
-    searchParams: URLSearchParams;
-    pageData?: string;
+  isLoading?: boolean;
+  allCategories: string[];
+  filters: filterType[];
+  colors: string[];
+  sizes: string[];
+  setSortParams: (key: string, value: string) => void;
+  deleteSortParams: (key: string) => void;
+  searchParams: URLSearchParams;
+  pageData?: string;
 }
 
 interface filterType {
-    name: string;
+  name: string;
 }
 
 export interface FilterProps {
-    setSortParams: (key: string, value: string) => void;
-    deleteSortParams: (key: string) => void;
-    params: string | null;
-    searchParams: URLSearchParams;
+  setSortParams: (key: string, value: string) => void;
+  deleteSortParams: (key: string) => void;
+  params: string | null;
+  searchParams: URLSearchParams;
 }
 
 // prodcts
 export interface ProductType {
-    title: string;
-    price: number;
-    category: string;
-    subCategory?: string;
-    tags: string;
-    registrationDate?:number;
-    colors: string[];
-    description: string;
-    options: string;
-    id: number;
-    image: string[];
-    details?: string;
-    highlights?: string[];
+  title: string; // Galaxy Flip6
+  model: string; // IJSF23-FAIONSD
+  storage?: string; // 128GB
+  referencePrice: number | number[]; // 130000
+  promotionalPrice: number | number[]; // 1300000
+  category: string; // smartphone
+  subCategory?: string;
+  tags: string; // flip-series
+  registrationDate?: number;
+  colors: string[]; // ["black", "white", "violet"],
+  description: string; // The latest flip-serires
+  options?: string;
+  id: number; // 1
+  image: string[]; // [~,~,~]
+  details?: string;
+  highlights?: string[]; // [~,~,~]
 }
